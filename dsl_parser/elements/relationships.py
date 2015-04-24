b4c4e7dee9df9d8f1c936175b714e3c4433d39d2
Dan Kilman <dankilman@gmail.com>
2015-04-24 13:47:21 +0300

    CFY-1546 port relationship types parsing

--- dsl_parser/elements/relationships.py
+++ dsl_parser/elements/relationships.py
@@ -1,6 +1,12 @@
+import copy
+
+from dsl_parser import parser as old_parser
+
+import plugins as _plugins
+import parser
 import properties
 import operation
-from elements import Element, Leaf, Dict, List
+from elements import DictElement, Element, Leaf, Dict, List
 
 
 class DerivedFrom(Element):
@@ -32,8 +38,28 @@ class Relationship(Element):
             'version': '1_0'
         }
     }
+    requires = {
+        'inputs': [parser.Requirement('resource_base', required=False)],
+        _plugins.Plugins: [parser.Requirement('plugins', parsed=True)]
+    }
+
+    def parse(self, plugins, resource_base):
+        relationship_type = self.initial_value
+        relationship_type_name = self.name
+        complete_relationship = old_parser._extract_complete_relationship_type(
+            relationship_type=relationship_type,
+            relationship_type_name=relationship_type_name,
+            relationship_types=self.ancestor(Relationships).initial_value
+        )
+
+        old_parser._validate_relationship_fields(relationship_type, plugins,
+                                      relationship_type_name,
+                                      resource_base)
+        complete_rel_obj_copy = copy.deepcopy(complete_relationship)
+        complete_rel_obj_copy['name'] = relationship_type_name
+        return complete_rel_obj_copy
 
 
-class Relationships(Element):
+class Relationships(DictElement):
 
     schema = Dict(type=Relationship, version='1_0')
