58eb495ac0ade1e422e86e98280dc5235a616698
Dan Kilman <dankilman@gmail.com>
2015-04-27 23:47:36 +0300

    CFY-1546 imports code cleanup

--- dsl_parser/elements/imports.py
+++ dsl_parser/elements/imports.py
@@ -26,6 +26,16 @@ from dsl_parser.elements.elements import (Element,
                                           Leaf,
                                           List)
 
+MERGE_NO_OVERRIDE = set([
+    old_parser.INTERFACES,
+    old_parser.NODE_TYPES,
+    old_parser.PLUGINS,
+    old_parser.WORKFLOWS,
+    old_parser.RELATIONSHIPS,
+    old_parser.POLICY_TYPES,
+    old_parser.GROUPS,
+    old_parser.POLICY_TRIGGERS])
+
 
 class Import(Element):
 
@@ -37,15 +47,24 @@ class Imports(Element):
     schema = List(type=Import)
 
 
+class ImportLoader(Element):
+
+    schema = Leaf(type=str)
+
+
 class ImportsLoader(Element):
 
-    schema = List(type=Import)
+    schema = List(type=ImportLoader)
     provides = ['resource_base']
     requires = {
         'inputs': ['main_blueprint',
                    'resources_base_url',
                    'blueprint_location']
     }
+
+    def __init__(self, *args, **kwargs):
+        super(ImportsLoader, self).__init__(*args, **kwargs)
+        self.resource_base = None
 
     def validate(self, **kwargs):
         imports = [i.value for i in self.children()]
@@ -61,7 +80,6 @@ class ImportsLoader(Element):
               main_blueprint,
               resources_base_url,
               blueprint_location):
-        self.resource_base = None
         if blueprint_location:
             blueprint_location = _dsl_location_to_url(
                 dsl_location=blueprint_location,
@@ -97,10 +115,8 @@ def _get_resource_location(resource_name,
                            resources_base_url,
                            current_resource_context=None):
     # Already url format
-    if resource_name.startswith('http:') \
-            or resource_name.startswith('https:') \
-            or resource_name.startswith('file:') \
-            or resource_name.startswith('ftp:'):
+    url_parts = resource_name.split(':')
+    if url_parts[0] in ['http', 'https', 'file', 'ftp']:
         return resource_name
 
     # Points to an existing file
@@ -116,95 +132,31 @@ def _get_resource_location(resource_name,
 
     if resources_base_url:
         return resources_base_url + resource_name
+    else:
+        return None
 
 
 def _combine_imports(parsed_dsl, dsl_location, resources_base_url):
-    def _merge_into_dict_or_throw_on_duplicate(from_dict, to_dict,
-                                               top_level_key, path):
-        for _key, _value in from_dict.iteritems():
-            if _key not in to_dict:
-                to_dict[_key] = _value
-            else:
-                path.append(_key)
-                raise exceptions.DSLParsingLogicException(
-                    4, 'Failed on import: Could not merge {0} due to conflict '
-                       'on path {1}'.format(top_level_key, ' --> '.join(path)))
-
-    merge_no_override = set([
-        old_parser.INTERFACES,
-        old_parser.NODE_TYPES,
-        old_parser.PLUGINS,
-        old_parser.WORKFLOWS,
-        old_parser.RELATIONSHIPS,
-        old_parser.POLICY_TYPES,
-        old_parser.GROUPS,
-        old_parser.POLICY_TRIGGERS])
-
     combined_parsed_dsl = copy.deepcopy(parsed_dsl)
 
     if old_parser.IMPORTS not in parsed_dsl:
         return combined_parsed_dsl
 
-    dsl_version = parsed_dsl[version.VERSION]
-
-    ordered_imports_list = []
-    _build_ordered_imports_list(parsed_dsl, ordered_imports_list,
-                                dsl_location,
-                                resources_base_url)
+    ordered_imports = _build_ordered_imports(parsed_dsl,
+                                             dsl_location,
+                                             resources_base_url)
     if dsl_location:
-        ordered_imports_list = ordered_imports_list[1:]
+        ordered_imports = ordered_imports[1:]
 
-    for single_import in ordered_imports_list:
-        try:
-            # (note that this check is only to verify nothing went wrong in
-            # the meanwhile, as we've already read
-            # from all imported files earlier)
-            with contextlib.closing(urllib2.urlopen(single_import)) as f:
-                parsed_imported_dsl = old_parser._load_yaml(
-                    f, 'Failed to parse import {0}'.format(single_import))
-        except urllib2.URLError, ex:
-            error = exceptions.DSLParsingLogicException(
-                13, 'Failed on import - Unable to open import url {0}; {1}'
-                    .format(single_import, ex.message))
-            error.failed_import = single_import
-            raise error
-
-        if version.VERSION in parsed_imported_dsl:
-            imported_dsl_version = parsed_imported_dsl[version.VERSION]
-            if imported_dsl_version != dsl_version:
-                raise exceptions.DSLParsingLogicException(
-                    28, "An import uses a different "
-                        "tosca_definitions_version than the one defined in "
-                        "the main blueprint's file: main blueprint's file "
-                        "version is {0}, import with different version is {"
-                        "1}, version of problematic import is {2}"
-                        .format(dsl_version,
-                                single_import,
-                                imported_dsl_version))
-            # no need to keep imported dsl's version - it's only used for
-            # validation against the main blueprint's version
-            del parsed_imported_dsl[version.VERSION]
-
-        # combine the current file with the combined parsed dsl
-        # we have thus far
-        for key, value in parsed_imported_dsl.iteritems():
-            if key == old_parser.IMPORTS:  # no need to merge those..
-                continue
-            if key not in combined_parsed_dsl:
-                # simply add this first level property to the dsl
-                combined_parsed_dsl[key] = value
-            else:
-                if key in merge_no_override:
-                    # this section will combine dictionary entries of the top
-                    # level only, with no overrides
-                    _merge_into_dict_or_throw_on_duplicate(
-                        value, combined_parsed_dsl[key], key, [])
-                else:
-                    # first level property is not white-listed for merge -
-                    # throw an exception
-                    raise exceptions.DSLParsingLogicException(
-                        3, 'Failed on import: non-mergeable field: "{0}"'
-                           .format(key))
+    dsl_version = parsed_dsl[version.VERSION]
+    for imported in ordered_imports:
+        import_url = imported['import']
+        parsed_imported_dsl = imported['parsed']
+        _validate_and_remove_dsl_version_if_exists(dsl_version,
+                                                   import_url,
+                                                   parsed_imported_dsl)
+        _merge_parsed_into_combined(combined_parsed_dsl,
+                                    parsed_imported_dsl)
 
     # clean the now unnecessary 'imports' section from the combined dsl
     if old_parser.IMPORTS in combined_parsed_dsl:
@@ -212,17 +164,22 @@ def _combine_imports(parsed_dsl, dsl_location, resources_base_url):
     return combined_parsed_dsl
 
 
-def _build_ordered_imports_list(parsed_dsl,
-                                ordered_imports_list,
-                                current_import,
-                                resources_base_url):
-    def _build_ordered_imports_list_recursive(_parsed_dsl, _current_import):
+def _build_ordered_imports(parsed_dsl,
+                           dsl_location,
+                           resources_base_url):
+    ordered_imports = []
+
+    def _build_ordered_imports_recursive(_parsed_dsl, _current_import):
         if _current_import is not None:
-            ordered_imports_list.append(_current_import)
+            ordered_imports.append({
+                'import': _current_import,
+                'parsed': _parsed_dsl,
+            })
 
         if old_parser.IMPORTS not in _parsed_dsl:
             return
 
+        imports = [i['import'] for i in ordered_imports]
         for another_import in _parsed_dsl[old_parser.IMPORTS]:
             import_url = _get_resource_location(another_import,
                                                 resources_base_url,
@@ -233,14 +190,14 @@ def _build_ordered_imports_list(parsed_dsl,
                         'import {0}'.format(another_import))
                 ex.failed_import = another_import
                 raise ex
-            if import_url not in ordered_imports_list:
+            if import_url not in imports:
                 try:
                     with contextlib.closing(urllib2.urlopen(import_url)) as f:
                         imported_dsl = old_parser._load_yaml(
                             f, 'Failed to parse import {0} (via {1})'
                                .format(another_import, import_url))
-                    _build_ordered_imports_list_recursive(imported_dsl,
-                                                          import_url)
+                    _build_ordered_imports_recursive(imported_dsl,
+                                                     import_url)
                 except urllib2.URLError, ex:
                     ex = old_parser.DSLParsingLogicException(
                         13, 'Failed on import - Unable to open import url '
@@ -248,4 +205,59 @@ def _build_ordered_imports_list(parsed_dsl,
                     ex.failed_import = import_url
                     raise ex
 
-    _build_ordered_imports_list_recursive(parsed_dsl, current_import)
+    _build_ordered_imports_recursive(parsed_dsl, dsl_location)
+    return ordered_imports
+
+
+def _validate_and_remove_dsl_version_if_exists(dsl_version,
+                                               import_url,
+                                               parsed_imported_dsl):
+    if version.VERSION in parsed_imported_dsl:
+        imported_dsl_version = parsed_imported_dsl[version.VERSION]
+        if imported_dsl_version != dsl_version:
+            raise exceptions.DSLParsingLogicException(
+                28, "An import uses a different "
+                    "tosca_definitions_version than the one defined in "
+                    "the main blueprint's file: main blueprint's file "
+                    "version is {0}, import with different version is {"
+                    "1}, version of problematic import is {2}"
+                    .format(dsl_version,
+                            import_url,
+                            imported_dsl_version))
+        # no need to keep imported dsl's version - it's only used for
+        # validation against the main blueprint's version
+        del parsed_imported_dsl[version.VERSION]
+
+
+def _merge_parsed_into_combined(combined_parsed_dsl, parsed_imported_dsl):
+    # combine the current file with the combined parsed dsl
+    # we have thus far
+    for key, value in parsed_imported_dsl.iteritems():
+        if key == old_parser.IMPORTS:  # no need to merge those..
+            continue
+        if key not in combined_parsed_dsl:
+            # simply add this first level property to the dsl
+            combined_parsed_dsl[key] = value
+        else:
+            if key in MERGE_NO_OVERRIDE:
+                # this section will combine dictionary entries of the top
+                # level only, with no overrides
+                _merge_into_dict_or_throw_on_duplicate(
+                    value, combined_parsed_dsl[key], key)
+            else:
+                # first level property is not white-listed for merge -
+                # throw an exception
+                raise exceptions.DSLParsingLogicException(
+                    3, 'Failed on import: non-mergeable field: "{0}"'
+                       .format(key))
+
+
+def _merge_into_dict_or_throw_on_duplicate(from_dict, to_dict,
+                                           top_level_key):
+    for key, value in from_dict.iteritems():
+        if key not in to_dict:
+            to_dict[key] = value
+        else:
+            raise exceptions.DSLParsingLogicException(
+                4, 'Failed on import: Could not merge {0} due to conflict '
+                   'on {1}'.format(top_level_key, key))
